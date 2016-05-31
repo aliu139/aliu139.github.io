15e3247c11aad122a1e5bcb592c824607040d702
aliu139 <a.liu@hotmail.com>
2016-05-31 10:50:36 -0400

    adding routing

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -1,17 +1,13 @@
-var slideout = new Slideout({
-      'panel': document.getElementById('panel'),
-      'menu': document.getElementById('menu'),
-      'padding': 256,
-      'tolerance': 70
-});
+$(document).ready(function() {
+	slideout = new Slideout({
+	      'panel': document.getElementById('panel'),
+	      'menu': document.getElementById('menu'),
+	      'padding': 256,
+	      'tolerance': 70
+	});
 
-// Toggle button
-document.querySelector('.js-slideout-toggle').addEventListener('click', function() {
-  slideout.toggle();
-  $('.js-slideout-toggle').hide();
+	document.querySelector('.js-slideout-toggle2').addEventListener('click', function(){
+	  slideout.toggle();
+	  $('.js-slideout-toggle').css('visibility','visible');
+	});
 });
-
-document.querySelector('.js-slideout-toggle2').addEventListener('click', function(){
-  slideout.toggle();
-  $('.js-slideout-toggle').show();
-})
